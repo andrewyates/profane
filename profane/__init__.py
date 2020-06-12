03a0cecd99818eb4d903d4ff82a5874d5b6ab0c7
Andrew Yates <andrew.yates@gmail.com>
2020-06-12 19:12:26 +0200

    version bump

--- profane/__init__.py
+++ profane/__init__.py
@@ -5,7 +5,7 @@ from profane.exceptions import PipelineConstructionError, InvalidConfigError, In
 from profane.frozendict import FrozenDict
 from profane.sql import DBManager
 
-__version__ = "0.1.3"
+__version__ = "0.1.5"
 
 constants = profane.base.constants
 module_registry = profane.base.module_registry
