1e1313a87077d4cec4139646f30ad2dd6f074c81
Andrew Yates <andrew.yates@gmail.com>
2020-06-18 12:06:40 +0200

    accept config files specified via 'file=cfg.txt' or 'module.file=cfg.txt'

--- tests/test_config_string.py
+++ tests/test_config_string.py
@@ -1,3 +1,4 @@
+import os
 import pytest
 
 from profane.cli import config_list_to_dict
@@ -25,3 +26,23 @@ def test_config_string_to_dict():
     with pytest.raises(ValueError):
         args = [".invalid=1"]
         config_list_to_dict(args)
+
+
+def test_config_string_with_files_to_dict(tmpdir):
+    mainfn = os.path.join(tmpdir, "main.txt")
+    with open(mainfn, "wt") as f:
+        print("main=24", file=f)
+
+    foofn = os.path.join(tmpdir, "foo.txt")
+    with open(foofn, "wt") as f:
+        print("test1=20", file=f)
+        print("submod1.test1=21", file=f)
+        print("submod1.submod2.test1=22", file=f)
+        print("test3=extra", file=f)
+        print(f"file={mainfn}", file=f)
+
+    args = ["foo.test1=1", f"foo.file={foofn}", "main=42", f"file={mainfn}"]
+    assert config_list_to_dict(args) == {
+        "foo": {"test1": "20", "test3": "extra", "main": "24", "submod1": {"test1": "21", "submod2": {"test1": "22"}}},
+        "main": "24",
+    }
