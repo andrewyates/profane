6b1c345a01011dc156339d49e2bcd242fd745829
Nima Sadri <nimasadri11@gmail.com>
2021-12-10 05:23:06 -0500

    support for YAML files (#21)
    * added support for YAML files
    * added yaml unit test
    * added pyyaml dep to setup.py + relaxed pyyaml version requirement

--- profane/cli.py
+++ profane/cli.py
@@ -1,5 +1,7 @@
 import os
 from shlex import shlex
+import yaml
+import collections
 
 
 def config_string_to_dict(s):
@@ -16,7 +18,34 @@ def config_list_to_dict(l):
     return d
 
 
-def _dot_to_dict(d, k, v):
+def _recursive_update(ori_dict, new_dict):
+    for k in new_dict:
+        if k not in ori_dict:
+            ori_dict[k] = new_dict[k]
+        elif not isinstance(ori_dict[k], dict):
+            ori_dict[k] = new_dict[k]
+        else:
+            ori_value, new_value = ori_dict[k], new_dict[k]
+            ori_dict[k] = _recursive_update(ori_value, new_value)
+    return ori_dict
+
+
+def _load_yaml(fn):
+    with open(fn) as f:
+        config = yaml.safe_load(f)
+    return config
+
+def _flatten(d, parent_key='', sep='.'):
+    items = []
+    for k, v in d.items():
+        new_key = parent_key + sep + k if parent_key else k
+        if isinstance(v, collections.abc.MutableMapping):
+            items.extend(_flatten(v, new_key, sep=sep))
+        else:
+            items.append(str(new_key) + '=' + str(v))
+    return list(items)
+
+def _dot_to_dict(d, k, v, DEL=''):
     if k.startswith(".") or k.endswith("."):
         raise ValueError(f"invalid path: {k}")
 
@@ -26,7 +55,8 @@ def _dot_to_dict(d, k, v):
         remaining_path = ".".join(path[1:])
 
         d.setdefault(current_k, {})
-        _dot_to_dict(d[current_k], remaining_path, v)
+        
+        _dot_to_dict(d[current_k], remaining_path, v, DEL=DEL+"  ")
     elif k.lower() == "file":
         lst = _config_file_to_list(v)
         for new_k, new_v in _config_list_to_pairs(lst):
@@ -57,13 +87,17 @@ def _config_list_to_pairs(l):
 
 def _config_file_to_list(fn):
     lst = []
-    with open(os.path.expanduser(fn), "rt") as f:
-        for line in f:
-            lex = shlex(line)
-            lex.whitespace = ""
-            kvs = "".join(list(lex))
-
-            for kv in kvs.strip().split():
-                lst.append(kv)
+    ext = os.path.splitext(fn)[1]
+    if ext == '.yaml':
+        yaml_list = _flatten(_load_yaml(fn))
+        lst.extend(yaml_list)
+    else:
+        with open(os.path.expanduser(fn), "rt") as f:
+            for line in f:
+                lex = shlex(line)
+                lex.whitespace = ""
+                kvs = "".join(list(lex))
+                for kv in kvs.strip().split():
+                    lst.append(kv)
 
     return lst
